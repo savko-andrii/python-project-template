156264c6891ebe437b067dfcc9b1e7e3ee8e275a
savko-andrii <savaandrej631@gmail.com>
2025-07-25 23:20:53 +0300

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,7 @@
-# this file is *not* meant to cover or endorse the use of nox or pytest or
-# testing in general,
-#
-#  It's meant to show the use of:
-#
-#  - check-manifest
-#     confirm items checked into vcs are in your sdist
-#  - readme_renderer (when using a reStructuredText README)
-#     confirms your long_description will render correctly on PyPI.
-#
-#  and also to help confirm pull requests to this project.
-
 import os
-
 import nox
 
 nox.options.sessions = ["lint"]
-
-# Define the minimal nox version required to run
 nox.needs_version = ">= 2024.3.2"
 
 
@@ -32,15 +17,12 @@ def lint(session):
 @nox.session
 def build_and_check_dists(session):
     session.install("build", "check-manifest >= 0.42", "twine")
-    # If your project uses README.rst, uncomment the following:
-    # session.install("readme_renderer")
-
     session.run("check-manifest", "--ignore", "noxfile.py,tests/**")
     session.run("python", "-m", "build")
     session.run("python", "-m", "twine", "check", "dist/*")
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python="3.11")
 def tests(session):
     session.install("pytest")
     build_and_check_dists(session)
